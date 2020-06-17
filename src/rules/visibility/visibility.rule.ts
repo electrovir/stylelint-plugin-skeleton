2437e915c6e158b6de8993297d87ada5bf3d3f45
electrovir <electrovir@users.noreply.github.com>
2020-06-17 06:30:34 -0600

    add auto fix skeleton

--- src/rules/visibility/visibility.rule.ts
+++ src/rules/visibility/visibility.rule.ts
@@ -3,13 +3,20 @@ import {createRule, createRuleMessage} from '../../rule';
 
 const ruleName = 'no-visibility-allowed';
 
-const rule: Plugin = (primaryOption, secondaryOptions, context = {}) => {
+const rule: Plugin = (
+    primaryOption,
+    secondaryOptions,
+    context: {fix: boolean; newline: string} = {fix: false, newline: '\n'},
+) => {
     return (root, result) => {
         if (!primaryOption) {
             return;
         }
 
         root.walkDecls(decl => {
+            if (context.fix) {
+                decl.remove();
+            }
             if (decl.prop === 'visibility') {
                 utils.report({
                     result,
